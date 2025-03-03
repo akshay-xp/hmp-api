fd7c2d2e24ea7cdcdf3a49e30813801f416aeb5a
Akshay PS <psakshay2022@gmail.com>
2025-03-03 22:15:33 +0530

    add helmet to add security headers

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT');
   const webUrl = config.get<string>('WEB_URL');
+
+  // set security HTTP headers
+  app.use(helmet());
 
   // enable CORS
   app.enableCors({
